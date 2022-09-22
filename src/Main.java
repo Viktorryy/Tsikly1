61f5e1f2cd5c9d5ca10da425662dfc8212c2712b
Viktorryy <viktory_o@rambler.ru>
2022-09-22 21:15:55 +0400

    // Домашнее задание -2         //Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // Домашнее задание -2
-        //Задание 2
+        //Задание 3
 
-        int ii = 0;
+        int ii = 1;
+        System.out.println("1");
 
-        for(int i = 1; i <= 14; i = i + 1) {
-            ii = ii + 7;
+        for(int i = 1; i <= 9; i = i + 1) {
+            ii = ii * 2;
             System.out.println(ii);
+
         }
 
     }

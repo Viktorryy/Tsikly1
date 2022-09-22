6b6957bcd5e5d8e6acfdaa12c3e2006f78a7467a
Viktorryy <viktory_o@rambler.ru>
2022-09-22 20:27:33 +0400

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 1
+        //Задание 2
 
-    for(int i = 1; i <= 10; i++) {
+    for(int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
 

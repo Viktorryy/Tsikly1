80a56ad3f11e28837d425f06cdc98cacde5a75a5
Viktorryy <viktory_o@rambler.ru>
2022-09-22 20:37:25 +0400

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 3
+        //Задание 4
 
-    for(int i = 0; i <= 17; i++) {
-        if (i % 2 == 0) {
-            System.out.println(i);
-        }
-
-
+    for(int i = 10; i >= -10; i--) {
+        System.out.println(i);
         }
 
     }
